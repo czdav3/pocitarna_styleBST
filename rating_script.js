9c64dddc80eee27919236e7a22c4c8ab4d93636e
czdav3 <62949669+czdav3@users.noreply.github.com>
2020-05-18 11:39:29 +0200

    Create rating_script.js

---
+++ rating_script.js
@@ -0,0 +1,18 @@
+ console.log("Načten hodnotící script.");
+            function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+        document.querySelectorAll(".star.star-off.show-tooltip.is-active.productRatingAction")[4].click();
+            console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {1}");
+    eventFire(document.querySelectorAll(".star.star-on.show-tooltip")[4], 'click');
+         console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {2}");
+        eventFire(document.querySelectorAll(".star.star-off.show-tooltip.is-active.productRatingAction")[4], 'click');
+    console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {3}");
+        console.log("HODNOTÍCÍ SCRIPT: Vše naklikáno.");
