e4aa54233afce919d618ab8101629a97ce722ff8
czdav3 <62949669+czdav3@users.noreply.github.com>
2020-05-18 13:56:34 +0200

    Creating new script
    stock-disabled javaScript add...

---
+++ stock-disabled.js
@@ -0,0 +1,10 @@
+console.log("-------------------------------");
+console.log("Externí sklad IMMAX .disabled = false;");
+var inputName = document.getElementsByName('stocksAmount[5]')[0];
+var inputLocation = document.getElementsByName('stocksLocation[5]')[0];
+inputName.disabled = true;
+inputLocation.disabled = true;
+console.log(inputName);
+console.log(inputLocation);
+console.log("Externí sklad IMMAX .disabled = true;");
+console.log("-------------------------------");
