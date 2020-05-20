82e27c28943fff5afb7680a77ed8067fa4ab698d
czdav3 <62949669+czdav3@users.noreply.github.com>
2020-05-20 09:03:32 +0200

    edit 0,2
    předělaný script kompletně

--- rating_script.js
+++ rating_script.js
@@ -1,18 +1,10 @@
- console.log("Rate ready!");
-            function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
+var activeRate = document.getElementsByClassName('is-active productRatingAction')[4];
 
-        document.querySelectorAll(".star.star-off.show-tooltip.is-active.productRatingAction")[4].click();
-            console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {1}");
-    eventFire(document.querySelectorAll(".star.star-on.show-tooltip")[4], 'click');
-         console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {2}");
-        eventFire(document.querySelectorAll(".star.star-off.show-tooltip.is-active.productRatingAction")[4], 'click');
-    console.log("HODNOTÍCÍ SCRIPT: Klikl jsme na {3}");
-        console.log("HODNOTÍCÍ SCRIPT: Vše naklikáno.");
+if (activeRate){
+    console.log(activeRate);
+    activeRate.dispatchEvent(new MouseEvent("click"));
+    console.log("Kliknul jsem!");
+}
+else{
+    console.log("Není nic ke kliknutí!");
+}
