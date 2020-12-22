825986ec3bc1f237a2b133ec73fb7fb83fc5cfe4
CzDaV3 <dav3.cz@gmail.com>
2020-12-22 17:37:25 +0100

    Revert "Update serv-disabled.js"
    This reverts commit 97a5a809c15ef76f2ef9917753cb8c193275be65.

--- serv-disabled.js
+++ serv-disabled.js
@@ -6,15 +6,9 @@ var htmlString = $('body').html().toString();
 var index = htmlString.indexOf("_SERV");
 
 if (index != -1)
-    {var inputName = document.getElementsByName('stocksAmount[1]')[0];
-    var inputLocation = document.getElementsByName('stocksLocation[1]')[0];
+    alert("Warning this is a gene 1001");
 
-    inputName.disabled = true;
-    inputLocation.disabled = true;
-
-    console.log(inputName);
-    console.log(inputLocation);}
-
+console.log(htmlString);
 
 console.log("-------------------------------");
 console.log("Servisní blokace .disabled = false;");
