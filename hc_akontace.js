8d8f66a5cca9966fed2e8ab45115f660683b2427
czdav3 <62949669+czdav3@users.noreply.github.com>
2021-05-07 10:29:29 +0200

    Create hc_akontace.js

---
+++ hc_akontace.js
@@ -0,0 +1,17 @@
+console.log("---------------------------");
+console.log(" ");
+
+var akontace = document.getElementsByName('text3')[0].value;
+console.log(akontace);
+
+var table = document.getElementById("t-order-contact").getElementsByTagName("tbody")[0];
+var row = table.insertRow();
+var cell = row.insertCell();
+cell.colSpan = 3;
+var div = document.createElement('div');
+div.id = "hc_akontace_alert";
+div.innerHTML += '<b>POZOR!!!</b> Odeslat s dobírkou '+akontace+'!';
+cell.appendChild(div);
+
+console.log(" ");
+console.log("---------------------------");
